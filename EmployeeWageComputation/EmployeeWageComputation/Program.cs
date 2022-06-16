33ec20158d24aa689b1f7c7e6bcba03ea8f2e758
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-16 14:40:32 +0530

    [Rajesh] Added : Class Method & Variables

--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -6,8 +6,8 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            MonthlyWageFor100hrs monthlyWageFor100Hrs = new MonthlyWageFor100hrs();
-            monthlyWageFor100Hrs.CalculateEmpMonthlyWage();
+            ClassMethodVariables classMethodVariables = new ClassMethodVariables();
+            classMethodVariables.MethodVariables();
         }
     }
 }
