2f71900b55a9ef495b0bdcbdee2eb051ac07d757
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-16 12:25:09 +0530

    [Rajesh] Added : Part Time Employee Wage

--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -6,8 +6,8 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            EmpDailyWage obj = new EmpDailyWage();
-            obj.DailyWage();
+            PartTimeEmployee partTimeEmployee = new PartTimeEmployee();
+            partTimeEmployee.PartTimeWage();
         }
     }
 }
