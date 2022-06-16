6a407bb095c094aa5c8df902c6876d01daff1e44
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-16 12:08:41 +0530

    [Rajesh] Added : Employee Daily Wage

--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -6,8 +6,8 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            EmployeeAttendance prg = new EmployeeAttendance();
-            prg.CheckEmpPresentAbsent();
+            EmpDailyWage obj = new EmpDailyWage();
+            obj.DailyWage();
         }
     }
 }
