e137f78e433f47e10b0b69c8538f24d6206be676
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-17 16:36:09 +0530

    [Rajesh] Added : Manage Wage For Multiple Company using Array

---
+++ EmployeeWageComputation/EmployeeWageComputation/Company.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class Company
+    {
+        //Using instance variables instead of parameters as per Use Case requirements
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHrsInMonth;
+        public string company;
+        public int totalEmpWage;
+
+        public Company(string company, int empRatePerHour, int numOfWorkingDays, int maxHrsInMonth)
+        {
+            //Setting values in class variables from constructor
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHrsInMonth = maxHrsInMonth;
+        }
+        //setting value of total employee wage
+        public void SetTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        //Displaying wage by company
+        public string DisplayEmpWage()
+        {
+            return "Total Emp Wage for company : " + this.company + " is : " + this.totalEmpWage;
+        }
+    }
+}
+
--- EmployeeWageComputation/EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/EmployeeWageComputation/Program.cs
@@ -5,10 +5,15 @@
     {
         public static void Main(String[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            SaveWageForEachCompany save = new SaveWageForEachCompany();
-            save.SaveForEachCompany();
+            Console.WriteLine("Welcome to Employee Wage Computation Program");           
+            
+            EmpWageArray empWageBuilder = new EmpWageArray();
 
+            //Passing values to AddCompanyEmpWage method for further calculation
+            empWageBuilder.AddCompanyEmpWage("JEEP", 32, 22, 110);
+            empWageBuilder.AddCompanyEmpWage("TATA", 38, 20, 100);
+
+            empWageBuilder.ComputeEmpWage();
         }
     } 
 }
